aafa3561b174cecc4dbf26ee7ff7422a9ceaf7c4
tonilloyds <toni@lloyds.design>
2022-03-26 15:33:53 +0100

    test ids

--- src/MenuItem.tsx
+++ src/MenuItem.tsx
@@ -2,20 +2,26 @@ import { MenuItemProps } from "./menuConfigType";
 
 export const MenuItem = ({ item, isOpen, toggleState }: MenuItemProps) => {
   return (
-    <>
+    <div data-test-id={`first-level-${item.title}`}>
       <p>{item.title}</p>
       {item.subItems?.length && (
         <>
-          <button onClick={toggleState}>{isOpen ? "Hide" : "Expand"}</button>
+          <button data-test-id={`button-${item.title}`} onClick={toggleState}>
+            {isOpen ? "Hide" : "Expand"}
+          </button>
           {isOpen && (
-            <ul>
+            <ul data-test-id={`ul-${item.title}`}>
               {item.subItems.map((subitem) => {
-                return <li>{subitem}</li>;
+                return (
+                  <li data-test-id={`li-${item.title}-${subitem}`}>
+                    {subitem}
+                  </li>
+                );
               })}
             </ul>
           )}
         </>
       )}
-    </>
+    </div>
   );
 };
