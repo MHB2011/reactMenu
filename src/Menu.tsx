3bc2d7554097cbc424a63df8c8fe1d6adb998a8d
tonilloyds <toni@lloyds.design>
2022-03-27 21:09:58 +0200

    cleanup

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,25 +1,20 @@
 import { useReducer } from "react";
 import { MenuItem } from "./MenuItem";
-import { MenuProps, MenuStateType } from "./menuConfigType";
+import { MenuProps } from "./menuConfigType";
 import { menuReducer } from "./menuReducer";
 import { MenuActionType } from "./MenuActionType";
+import { getInitialState } from "./getInitialState";
+import { isSubmenuOpen } from "./isSubmenuOpen";
 
 export const Menu = ({ menuConfig }: MenuProps) => {
-  const initialState: MenuStateType = menuConfig.map((menuItem) => {
-    return {
-      menuName: menuItem.title.toLowerCase(),
-      isOpen: false,
-    };
-  });
-
+  const initialState = getInitialState(menuConfig);
   const [menuState, dispatch] = useReducer(menuReducer, initialState);
 
   return (
     <div>
       {menuConfig.map((item) => {
-        const isOpen =
-          menuState.find((i) => i.menuName === item.title.toLowerCase())
-            ?.isOpen ?? false;
+        const menuItemTitle = item.title.toLowerCase();
+        const isOpen = isSubmenuOpen({ menuState, menuItemTitle });
 
         return (
           <MenuItem
